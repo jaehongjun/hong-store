6988f1029c2b1708408b2a15459d1994a52ac012
jaehongjun <gatsby0391@naver.com>
2019-02-09 11:27:44 +0900

    dynamicPage

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Link from "next/link";
 import Head from "next/head";
 import withLayout from "../lib/withLayout";
+import PostLink from "../components/PostLink";
 
 const Index = () => (
   <div>
     <Head>
       <title>INDEX | hong-store</title>
     </Head>
-    <h1> Hello from the index </h1>
-    <Link href={"/about"}>
-      <a>About page</a>
-    </Link>
+    <h1>Posts:</h1>
+    <ul>
+      <li>
+        <PostLink title={"Something"} />
+      </li>
+      <li>
+        <PostLink title={"nothing"} />
+      </li>
+    </ul>
   </div>
 );
 
